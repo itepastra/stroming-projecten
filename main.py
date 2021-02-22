b24edff71b1c631eedaac583f5c59dd66011f03d
Tijs Aarts <tijs@voorwaarts.nl>
2021-02-22 20:11:46 +0100

    hamonische occilator...

--- main.py
+++ main.py
@@ -2,8 +2,31 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate as spi
 
-n = np.linspace(-5,5,500)
 
-plt.plot(n, n**8)
+def temp(y, t, k):
+    r,p = y
+    dydt= [p, -r/k]
+    return dydt
+
+
+k = 1e-5
+
+y0 = [0, 1]
+
+t = np.linspace(0,10,101)
+
+sol = spi.odeint(temp, y0, t, args=(k,))
+
+plt.plot(t, sol[:, 0], 'b', label='theta(t)')
+
+plt.plot(t, sol[:, 1], 'g', label='omega(t)')
+
+plt.legend(loc='best')
+
+plt.xlabel('t')
+
+plt.grid()
+
 plt.show()
