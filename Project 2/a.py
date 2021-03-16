8eb140d17159bb07d446e0f62894bc2dba5b4ad1
Tijs Aarts <tijs@voorwaarts.nl>
2021-03-16 11:17:40 +0100

    uuuuughg

--- Project 2/a.py
+++ Project 2/a.py
@@ -3,16 +3,29 @@ import matplotlib.pyplot as plt
 
 
 def Circle(origin, radius, n=1000):
-    thetas = np.linspace(0,2*np.pi,n, endpoint=False)
-    return origin + radius*np.exp(1j*thetas)
+    thetas = np.linspace(0, 2 * np.pi, n, endpoint=False)
+    return origin + radius * np.exp(1j * thetas)
+
 
 def Joukowski(b):
-    return lambda z : z + b**2/z
+    return lambda z: z + b**2 / z
+
 
 transformation = Joukowski(1)
 circle = Circle(-0.1 + 0.22j, 1.12)
 wingprofile = transformation(circle)
 
-plt.scatter(wingprofile.real, wingprofile.imag)
+#plotting the circle
+plt.scatter(circle.real,
+            circle.imag,
+            s=1,
+            c=np.linspace(0, 2 * np.pi, len(circle), endpoint=False),
+            cmap='gist_rainbow')
+#plotting the transformation of the circle
+plt.scatter(wingprofile.real,
+            wingprofile.imag,
+            s=1,
+            c=np.linspace(0, 2 * np.pi, len(wingprofile), endpoint=False),
+            cmap='gist_rainbow'),
 plt.axis("equal")
 plt.show()
